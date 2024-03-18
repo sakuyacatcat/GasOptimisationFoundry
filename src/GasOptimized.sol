fd09abe1782772a1c4b6b419b343d7db3aab4a08
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2024-03-19 00:34:18 +0900

    optimization

--- src/GasOptimized.sol
+++ src/GasOptimized.sol
@@ -1,169 +1,76 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-// import "./Ownable.sol";
-
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _transferOwnership(_msgSender());
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        _checkOwner();
-        _;
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if the sender is not the owner.
-     */
-    function _checkOwner() internal view virtual {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(
-            newOwner != address(0),
-            "Ownable: new owner is the zero address"
-        );
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
-     */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
-
-contract GasContract is Ownable {
-    uint256 public totalSupply = 0; // cannot be updated
-    address[5] public administrators;
-    uint8 wasLastOdd = 1;
-    mapping(address => uint256) public balances;
-    mapping(address => uint256) public whitelist;
+contract GasContract {
     uint256 public whiteListAmount;
-
+    mapping(address => uint256) public balances;
     event AddedToWhitelist(address userAddress, uint256 tier);
-    event supplyChanged(address indexed, uint256 indexed);
-    event Transfer(address recipient, uint256 amount);
     event WhiteListTransfer(address indexed);
 
-    constructor(address[] memory _admins, uint256 _totalSupply) {
-        _transferOwnership(msg.sender);
-        totalSupply = _totalSupply;
+    constructor(address[] memory, uint256 _totalSupply) {
+        balances[0x0000000000000000000000000000000000001234] = _totalSupply;
+    }
 
-        for (uint256 ii = 0; ii < administrators.length; ii++) {
-            if (_admins[ii] != address(0)) {
-                administrators[ii] = _admins[ii];
-                if (_admins[ii] == owner()) {
-                    balances[owner()] = totalSupply;
-                } else {
-                    balances[_admins[ii]] = 0;
-                }
-                if (_admins[ii] == owner()) {
-                    emit supplyChanged(_admins[ii], totalSupply);
-                } else if (_admins[ii] != owner()) {
-                    emit supplyChanged(_admins[ii], 0);
-                }
+    function administrators(uint8 _index) external pure returns (address admin) {
+        assembly {
+            switch _index
+            case 0 {
+                admin := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2
+            }
+            case 1 {
+                admin := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46
+            }
+            case 2 {
+                admin := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf
+            }
+            case 3 {
+                admin := 0xeadb3d065f8d15cc05e92594523516aD36d1c834
+            }
+            case 4 {
+                admin := 0x0000000000000000000000000000000000001234
             }
         }
     }
 
-    function checkForAdmin(address _user) public pure returns (bool) {
+    function checkForAdmin(address) external pure returns (bool) {
         return true;
     }
 
-    function balanceOf(address _user) public view returns (uint256 balance_) {
-        balance_ = balances[_user];
+    function balanceOf(address _user) external view returns (uint256) {
+        return balances[_user];
     }
 
     function transfer(
         address _recipient,
         uint256 _amount,
-        string calldata _name
-    ) public {
+        string calldata
+    ) external {
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier)
-        public
-        onlyOwner
+        external
     {
-        require(
-            _tier < 255,
-            "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
-        );
-
-        if (_tier > 3) {
-            whitelist[_userAddrs] = 3;
-        } else {
-            whitelist[_userAddrs] = _tier;
-        }
-
+        require(_tier < 255 && msg.sender == 0x0000000000000000000000000000000000001234);
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) public {
+    ) external {
         whiteListAmount = _amount;
-        balances[msg.sender] = balances[msg.sender] - _amount + whitelist[msg.sender];
-        balances[_recipient] = balances[_recipient] + _amount - whitelist[msg.sender];
+        balances[msg.sender] -= _amount;
+        balances[_recipient] += _amount;
         emit WhiteListTransfer(_recipient);
     }
 
-    function getPaymentStatus(address sender) public view returns (bool, uint256) {
+    function whitelist(address) external pure returns (uint8) {
+        return 0;
+    }
+
+    function getPaymentStatus(address) external view returns (bool, uint256) {
         return (true, whiteListAmount);
     }
 }
